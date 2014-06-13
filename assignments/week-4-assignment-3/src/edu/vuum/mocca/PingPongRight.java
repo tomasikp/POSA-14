1007cce81ba8823db387255059751c8b78a17b95
Piotr Tomasik <tomasikp@gmail.com>
2014-06-13 11:36:53 -0700

    ok

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
@@ -145,12 +145,12 @@ public class PingPongRight {
                                                           * TODO - You fill in
                                                           * here
                                                           */
-        		pingString,pingSema,pongSema,10);
+        		pingString,pingSema,pongSema,maxIterations);
         PlayPingPongThread pong = new PlayPingPongThread(/*
                                                           * TODO - You fill in
                                                           * here
                                                           */
-        		pongString,pongSema,pingSema,10);
+        		pongString,pongSema,pingSema,maxIterations);
 
         // TODO - Initiate the ping and pong threads, which will call
         // the run() hook method.
